261bb234df3d2f1fcce95e9c0785062af906d31d
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-07-03 02:08:10 +0200

    fix(server): user model stricter rules applied

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,8 +1,20 @@
 import mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({
-	name: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		minLength: 3,
+		maxLength: 16,
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 8,
+		maxLength: 20,
+	},
 });
 
 export const User = mongoose.model('User', schema);
