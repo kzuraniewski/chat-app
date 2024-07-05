0b8a820a05f6552f9489e483ba000187e4c66446
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-07-05 15:15:50 +0200

    refactor(server): auth procedure builder

--- server/src/router/builders.ts
+++ server/src/router/builders.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod';
 import authMiddleware from '../middleware/auth.middleware';
 import loggerMiddleware from '../middleware/logger.middleware';
 import { t } from '../trpc';
@@ -6,3 +7,10 @@ const baseProcedure = t.procedure.use(loggerMiddleware);
 
 export const publicProcedure = baseProcedure;
 export const protectedProcedure = baseProcedure.use(authMiddleware);
+
+export const authProcedure = publicProcedure.input(
+	z.object({
+		name: z.string(),
+		password: z.string(),
+	})
+);
--- server/src/router/register.procedure.ts
+++ server/src/router/register.procedure.ts
@@ -1,35 +1,27 @@
 import { TRPCError } from '@trpc/server';
-import { z } from 'zod';
 import { hashPassword, generateToken } from '../utils/auth';
-import { publicProcedure } from './appProcedures';
+import { authProcedure } from './builders';
 
-const registerProcedure = publicProcedure
-	.input(
-		z.object({
-			name: z.string(),
-			password: z.string(),
-		})
-	)
-	.mutation(async (opts) => {
-		const { name, password } = opts.input;
-		const { prisma } = opts.ctx;
+const registerProcedure = authProcedure.mutation(async (opts) => {
+	const { name, password } = opts.input;
+	const { prisma } = opts.ctx;
 
-		try {
-			const user = await prisma.user.create({
-				data: {
-					name,
-					passwordHash: hashPassword(password),
-				},
-			});
+	try {
+		const user = await prisma.user.create({
+			data: {
+				name,
+				passwordHash: hashPassword(password),
+			},
+		});
 
-			return generateToken(user);
-		} catch (error) {
-			throw new TRPCError({
-				code: 'BAD_REQUEST',
-				message: 'Cannot register user with given credentials',
-				cause: error,
-			});
-		}
-	});
+		return generateToken(user);
+	} catch (error) {
+		throw new TRPCError({
+			code: 'BAD_REQUEST',
+			message: 'Cannot register user with given credentials',
+			cause: error,
+		});
+	}
+});
 
 export default registerProcedure;
