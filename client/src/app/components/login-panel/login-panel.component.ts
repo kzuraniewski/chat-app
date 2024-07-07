bb22b93b823a40f055ea44b3ff013551383d8914
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-07-07 22:31:41 +0200

    feat(client): sign in panel

--- client/src/app/components/login-panel/login-panel.component.ts
+++ client/src/app/components/login-panel/login-panel.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { PasswordModule } from 'primeng/password';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
 	selector: 'app-login-panel',
 	standalone: true,
-	imports: [],
 	templateUrl: './login-panel.component.html',
+	imports: [
+		FormsModule,
+		ButtonModule,
+		InputTextModule,
+		CardModule,
+		FloatLabelModule,
+		PasswordModule,
+	],
 })
-export class LoginPanelComponent {}
+export class LoginPanelComponent {
+	username = 'root';
+	password = 'password';
+
+	constructor(private authService: AuthService) {}
+
+	handleLogin() {
+		this.authService.login(this.username, this.password);
+	}
+}
