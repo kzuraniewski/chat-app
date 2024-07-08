b902fd288a18183519f0ccce11a2f950bdc154cc
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-07-08 04:03:40 +0200

    feat(client): navigate to main page after authentication

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { TrpcService } from './trpc.service';
+import { Router } from '@angular/router';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AuthService extends TrpcService {
+	constructor(private router: Router) {
+		super();
+	}
+
 	async register(username: string, email: string, password: string) {
 		const token = await this.trpc.register.mutate({
 			username,
@@ -12,10 +17,12 @@ export class AuthService extends TrpcService {
 			password,
 		});
 		this.storeToken(token);
+		this.router.navigate(['/']);
 	}
 
 	async login(email: string, password: string) {
 		const token = await this.trpc.login.query({ email, password });
 		this.storeToken(token);
+		this.router.navigate(['/']);
 	}
 }
